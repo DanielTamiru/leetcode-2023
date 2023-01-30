6a06463536cea85c819177893380aeac391bfd28
Daniel Tamiru <danny.tamiru@gmail.com>
2023-01-29 20:01:08 -0500

    64

---
+++ 64_minimum_path_sum.py
@@ -0,0 +1,17 @@
+from copy import deepcopy
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        dp = deepcopy(grid)
+        
+        # Base Cases:
+        for i in reversed(range(m-1)): dp[i][-1] += dp[i+1][-1]
+        for j in reversed(range(n-1)): dp[-1][j] += dp[-1][j+1]
+
+        # General Case:
+        for i in reversed(range(m-1)):
+            for j in reversed(range(n-1)):
+                dp[i][j] = grid[i][j] + min(dp[i][j+1], dp[i+1][j])
+
+        return dp[0][0]
