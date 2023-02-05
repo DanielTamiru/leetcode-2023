5789b4c61d4869617574fe2bb51e4b023bcf3167
Daniel Tamiru <danny.tamiru@gmail.com>
2023-02-05 16:01:58 -0500

    update to 152

--- 152_maximum_product_subarray.py
+++ 152_maximum_product_subarray.py
@@ -1,5 +1,33 @@
 from typing import List
 
+# Note: brute force is quadratic in time. We are trying to do better than that
+
+# Observations:
+#     (1) Because these are integers, the product of a subarray of positive numbers
+#     increases with its length. 
+#     (2) Encountering a 0 would mean nothing preceding it mattered, effectively splitting
+#     the array into two subproblems.
+#     (3) Negative numbers flip the sign of the entire preceding subarray
+
+# Intuition:
+#     - To do better than O(n^2), let's try a linear pass through nums whereby we explore only
+#     prospectful subarray products and at the end of each iteration, we update our result if we
+#     found a larger subarray product (DP)
+#     - While the negatives may be tricky to work around, subarrays with large absolute products
+#     are the ones worth considering
+#     - Start off by considering just one subarray starting from nums[0], setting DP[0] to nums[0]
+#     - For each subsequent num, consider this recursive relation:
+#         DP[i] = max(DP[i], max product of subarrays that end with nums[i])
+#         Let's break the second parameter of that relation down. Depending on how large i is and
+#         the composition of nums, the subarrays ending with nums[i] can all have positive products,
+#         all have negative products, or often a combination of both.
+#         Knowing this and obervation (3), we have a fact that helps us restrict our subarray search space:
+#         * A longer positive subarray will always have a larger product AND a longer negative subarray will
+#          always a more negative product (whose sign can be flipped with just one addition number) *
+#         So, when keeping track of your most positive and negative subarray products so far (if they exist), 
+#         you only need to consider a maximum of two subarrays at a time: the longest ones.
+#     - The function below is an encoding of this logic where checking of for negative numbers is circumvented
+#       through the use of a max and min abstraction.
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
