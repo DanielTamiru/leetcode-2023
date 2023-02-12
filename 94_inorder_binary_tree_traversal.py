95563dfc3ee895332d17ac8b9056416912a5619e
Daniel Tamiru <danny.tamiru@gmail.com>
2023-02-12 11:24:55 -0500

    updated 94

--- 94_inorder_binary_tree_traversal.py
+++ 94_inorder_binary_tree_traversal.py
@@ -1,35 +1,39 @@
 from typing import Optional, List
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         """
-        In an iterative approach, you need to explicitly distinguish
+        Child -> Parent approach:
+    
+        Iteratively, you need to explicitly distinguish
         encountering a node initially vs ecountering one after having
-        explored any or all of its children. Below, this is accomplished by
-        mutating the tree, but depending on the context, that may be bad practice.
+        explored any or all of its children. The easiest way to accomplish
+        that would be by setting children to None, but mutating the tree
+        is generally considered bad practice.
+
+        Smart Traversal:
+
+        Instead of putting checks in place to ensure the right order is
+        achieved in the above approach, we literally perform an inorder
+        traversal by repeatedly going as left as we can, then going one
+        node to the right.
         """
-
-        res = []
-
-        node_stack = [root]
-        while node_stack:
-
-            node = node_stack.pop()
-            if not node: continue
-            left, right = node.left, node.right 
-
-            if left or right: node_stack.extend([right, node, left])
-            else: res.append(node.val)
-
-            node.left = node.right = None
+        result = []
+        stack = []
         
-        return res
-
+        while root or stack:
+            # Go as left as you can
+            while root is not None:
+                stack.append(root)
+                root = root.left
+            # Go right one
+            root = stack.pop()
+            result.append(root.val)
+            root = root.right
         
+        return result
