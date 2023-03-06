6a07bdf444ebdbf74d7d73e568881cc60d3eb8da
Daniel Tamiru <danny.tamiru@gmail.com>
2023-03-06 00:09:03 -0500

    3

---
+++ 3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        substr_char_positions = {}
+        cur_len, res = 0, 0
+
+        for i, char in enumerate(s):
+            past_i = substr_char_positions.get(char)
+            if past_i is not None:
+                if past_i in range(i - cur_len, i): cur_len = i - past_i 
+                else: del substr_char_positions[char]
+            else: cur_len += 1
+
+            substr_char_positions[char] = i
+            res = max(res, cur_len)
+
+
+        return res
